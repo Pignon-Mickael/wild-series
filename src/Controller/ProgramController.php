1e2183a8714c7ead85169bb09d650b4fcbbd61ea
Pignon-Mickael <mickaelpignon17@gmail.com>
2021-05-30 12:06:56 +0200

    ajout templates/program/show.html.twig + methode show -> ProgramController

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -6,15 +6,32 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
+/**
+ * @Route("/programs", name="program_")
+ */
 Class ProgramController extends AbstractController
 {
     /**
-     * @Route("/programs/", name="program_index")
+     * @Route("/", name="index")
+     * @return Response
      */
     public function index(): Response
     {
         return $this->render('program/index.html.twig', [
             'website' => 'Wild Séries',
+        ]);
+    }
+
+    /**
+     * @route("/{id}", requirements={"id"="\d+"}, methods={"GET"}, name="show")
+     * @param int $id
+     * @return Response
+     */
+    public function show(int $id): response
+    {
+        return $this->render('program/show.html.twig', [
+            'id' =>$id
         ]);
     }
 }
