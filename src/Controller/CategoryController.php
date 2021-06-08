130456ccfd252713966c1ae24c1ae8a1ea830acd
Pignon-Mickael <mickaelpignon17@gmail.com>
2021-06-08 14:16:16 +0200

    quete symfony 12

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -6,7 +6,9 @@ namespace App\Controller;
 
 use App\Entity\Category;
 use App\Entity\Program;
+use App\Form\CategoryType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -32,20 +34,41 @@ class CategoryController extends AbstractController
     }
 
     /**
+     * @route("/new", name="new")
+     * @param Request $request
+     * @return Response
+     */
+    public function new(Request $request): Response
+    {
+        $category = new Category();
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
+        if ($form->isSubmitted()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($category);
+            $entityManager->flush();
+            return $this->redirectToRoute('category_index');
+        }
+        return $this->render('category/new.html.twig', [
+            "form" => $form->createView(),
+        ]);
+    }
+
+    /**
      * @route("/{categoryName}", name="show")
      * @param string $categoryName
      * @return Response
      */
-    public function show(string $categoryName):Response
+    public function show(string $categoryName): Response
     {
         $category = $this->getDoctrine()
             ->getRepository(Category::class)
             ->findOneBy(['name' => $categoryName]);
 
-        if($category){
+        if ($category) {
             $programs = $this->getDoctrine()
                 ->getRepository(Program::class)
-                ->findBy(['category' => $category], ['id' => 'DESC'],3
+                ->findBy(['category' => $category], ['id' => 'DESC'], 3
                 );
         }
 
@@ -59,7 +82,6 @@ class CategoryController extends AbstractController
             'category' => $category,
             'programs' => $programs
         ]);
-
     }
 
 }
